b294a5d4c296629a5ac6dfe9305b151885264070
Thoratsahil <stsahil1717@gmail.com>
2023-11-01 17:17:53 +0530

    commit

---
+++ Basic.js
@@ -0,0 +1,33 @@
+let a=null;
+let b=232;
+let c="Paradice";
+let d=BigInt("3434");
+let e=true;
+let f=undefined;
+let g =Symbol("i'm symbol");
+ console.log(a,b,c,d,e,f,g);
+  console.log(typeof c)
+
+  const item={
+    "sahil": true,
+    "Sayluu": "everything",
+    "adesh": 22,
+     "sp": "Brother"
+  }
+  console.log(item["sp"])
+
+  let v="sahil"
+  let z=434
+  const sum=v+z
+  console.log(sum)
+
+  console.log(typeof z)
+
+  const st={
+    name:"sahil",
+    roll_no:23
+  }
+  
+  console.log(st);
+  st["add"]="pune"
+  console.log(st.name)
