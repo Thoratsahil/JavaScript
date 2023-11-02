40ee1c4102eba29415b9a5b6a64aa54234ac1730
Thoratsahil <stsahil1717@gmail.com>
2023-11-02 13:05:39 +0530

    loop commit

---
+++ loops.js
@@ -0,0 +1,43 @@
+/*
+let n=prompt("enter the limit");
+n=Number.parseInt(n);
+let sum=0;
+for(let i=1;i<n;i++)
+{
+   sum=sum+i;
+}
+console.log(sum);
+*/
+
+let obj={
+    harsh:99,
+    sayali:92,
+    sahil:88,
+    rushi:97
+}
+for(let a in obj)
+{
+    console.log(a);
+   console.log("marks of "+a+" are "+obj[a]);
+}
+
+/*let n=prompt("enter the no");
+n=Number.parseInt(n)
+let i=0;
+while(i<n)
+{
+    console.log("No"+i);
+    i++;
+
+    //do while
+
+    let n=prompt("enter the no");
+n=Number.parseInt(n)
+let i=10;
+do
+{
+    console.log("No"+i);
+    i++;
+}
+ while(i<n)
+*/
