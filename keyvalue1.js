5eb9f649c3116cb3081e13ba6fe1407ec61599b6
Thoratsahil <stsahil1717@gmail.com>
2024-01-11 19:44:21 +0530

    commit2

---
+++ keyvalue1.js
@@ -0,0 +1,9 @@
+//create a const object called product tp store info of amazon pen advertise.
+
+const product={
+    penName:"cello",
+    price:200,
+    rateing:3,
+    offer : 5 
+};
+console.log(product);
