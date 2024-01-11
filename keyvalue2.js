9e0bab766a3dace064437e01633683e0fe7eb192
Thoratsahil <stsahil1717@gmail.com>
2024-01-11 19:59:33 +0530

    commit3

---
+++ keyvalue2.js
@@ -0,0 +1,12 @@
+//create a const object called profile to store info abpout insta profile
+const profile={
+    idName:"Aparna",
+    isfollow:true,
+    isthreadAccount:true,
+    following:4,
+    followers:"599k",
+    post:13
+
+      
+};
+console.log(profile);
