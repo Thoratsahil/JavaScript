48281fd55e34dcbd2a45df1eaf0bb8eeb546ce20
Thoratsahil <stsahil1717@gmail.com>
2024-01-14 18:09:31 +0530

    commit

---
+++ numbergame.js
@@ -0,0 +1,9 @@
+let gameno=18;
+
+let userno=prompt("Enter a no between 1 to 20 ");
+
+while(userno != gameno){
+    userno=prompt("you entered wrong no ..please try again");
+}
+
+console.log("Congrats...you entered right no");
