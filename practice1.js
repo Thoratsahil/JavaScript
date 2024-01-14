66b87b14cd403017a4827b1fb16fd307b0197bb1
Thoratsahil <stsahil1717@gmail.com>
2024-01-14 13:14:05 +0530

    commit1

---
+++ practice1.js
@@ -0,0 +1,11 @@
+//its divisible from 5 or not
+let m=5;
+let as=prompt("enter any no");
+
+let result=as % m;
+if(result==0){
+console.log("result is",result);
+}
+else{
+    console.log("not divisible");
+}
