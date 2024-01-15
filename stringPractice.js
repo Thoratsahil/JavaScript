e2b8f7ad52d4d3a16e7beb0e04b7ba64da0b0ec9
Thoratsahil <stsahil1717@gmail.com>
2024-01-15 20:04:36 +0530

    commit1

---
+++ stringPractice.js
@@ -0,0 +1,7 @@
+//prompt the user to enter their full name,generate a username for them based on input  start username with  @ ,followed by their  full name and ending  with the  fullname with length
+
+let st=prompt("Enter your Name");
+//console.log(st);
+
+let ct="@" + st + st.length;
+console.log(ct);
