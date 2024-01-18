cb393013b00b9505c0397e3289ad437214981eb5
Thoratsahil <stsahil1717@gmail.com>
2024-01-18 12:25:34 +0530

    commit2

--- dom.js
+++ dom.js
@@ -7,6 +7,8 @@ let c=document.getElementsByClassName("cls");
 console.log(c);
 console.dir(c);
 
+
+//create h2 element and append the string like "from student" using javascript.
 let h=document.querySelector("h2");
 console.dir(h.innerText);
 
