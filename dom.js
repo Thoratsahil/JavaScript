bc4134048e9a3bddfb5fa0407f725b0757ce60c5
Thoratsahil <stsahil1717@gmail.com>
2024-01-18 12:16:30 +0530

    commit1

--- dom.js
+++ dom.js
@@ -6,3 +6,9 @@ console.dir(a);  */
 let c=document.getElementsByClassName("cls");
 console.log(c);
 console.dir(c);
+
+let h=document.querySelector("h2");
+console.dir(h.innerText);
+
+h.innerText=h.innerText + " from Students";
+console.dir(h.innerText);
