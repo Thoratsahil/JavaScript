5bdae6d3f05290a6c13b3a8a50071a9774c02113
Thoratsahil <stsahil1717@gmail.com>
2024-01-25 20:52:09 +0530

    cmt3

--- eventListener.js
+++ eventListener.js
@@ -1,6 +1,15 @@
 
 let modebtn=document.querySelector("#mode");    
 
+let currMode="light";
+
 modebtn.addEventListener("click",() =>{
-    console.log("you are trying to change mode");
+    if(currMode === "light"){
+        currMode="dark";
+        document.querySelector("body").style.backgroundColor="black";
+    } else{
+        currMode="light";
+        document.querySelector("body").style.backgroundColor="white";
+        
+    }
 })
